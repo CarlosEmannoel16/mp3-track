0dd6530c78bd2dca0e3fb63585d638d23d7d4cd1
Carlos Emannoel <carlosemannoel2019@gmail.com>
2023-02-02 15:54:38 -0300

    feat: centralized title

--- src/components/LeftBar.jsx
+++ src/components/LeftBar.jsx
@@ -3,7 +3,7 @@ import "./style.css";
 export function LeftBar({ children, play }) {
   return (
     <div className="leftBar" onClick={play}>
-      <h1 style={{ color: "#fff" }}>Spotify from depweb</h1>
+      <h1>Spotify from deepweb</h1>
       {children}
     </div>
   );
